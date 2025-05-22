7e94c6a6b210669f7c1bdbdfa0263b92db173306
Boris Moreno <boris_marco_moreno@hotmail.com>
2025-05-21 23:12:36 -0500

    netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,18 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import tailwindcss from "@tailwindcss/vite";
-import awsAmplify from 'astro-aws-amplify';
 
 import db from "@astrojs/db";
 
+import netlify from "@astrojs/netlify";
+
 // https://astro.build/config
 export default defineConfig({
-    vite: {
-        plugins: [tailwindcss()]
-    },
-    integrations: [db()],
-    output: 'server',
-    adapter: awsAmplify(),
+  vite: {
+      plugins: [tailwindcss()]
+  },
+
+  integrations: [db()],
+  output: 'server',
+  adapter: netlify(),
 });
